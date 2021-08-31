04dfdacc957b4afb5010c56db3de81a4a8ec3509
pirateskipper <blindfoldskipper@gmail.com>
2021-09-01 00:32:58 +0800

    logback.xml test Queue Logback configuration

---
+++ src/main/java/com/example/demo/logback/QueueLogback.java
@@ -0,0 +1,15 @@
+package com.example.demo.logback;
+
+import ch.qos.logback.core.UnsynchronizedAppenderBase;
+
+/**
+ * @author blindskipper
+ */
+public class QueueLogback extends UnsynchronizedAppenderBase {
+
+    @Override
+    protected void append(Object o) {
+
+    }
+
+}
