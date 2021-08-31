490c70ad0bf842c1b9b9010a6e3fe67d59bb8805
pirateskipper <blindfoldskipper@gmail.com>
2021-09-01 00:13:15 +0800

    logback.xml test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,17 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+
     public static void main(String[] args) {
+        log.info("application is started...");
         SpringApplication.run(DemoApplication.class, args);
     }
 
